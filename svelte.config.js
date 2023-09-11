60d84d607126549c5d7f0f64022f859466243f65
rajitha123k <rajithabhavani.kantheti@gmail.com>
2023-09-11 19:10:17 -0400

    deployment changes

--- svelte.config.js
+++ svelte.config.js
@@ -8,10 +8,7 @@ const config = {
   kit: {
     adapter: adapter(),
     paths: {
-      base:
-        process.env.NODE_ENV === "production"
-          ? "/virtual-scheduling-assistant"
-          : "",
+      base: process.env.NODE_ENV === "production" ? "" : "",
     },
   },
 };
