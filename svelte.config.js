64d23f90c75fd8c0aea42f9169607e0340ca78ac
rajitha123k <rajithabhavani.kantheti@gmail.com>
2023-09-12 13:12:24 -0400

    final deployment changes

--- svelte.config.js
+++ svelte.config.js
@@ -1,19 +1,17 @@
 import adapter from "@sveltejs/adapter-static";
-import { vitePreprocess } from "@sveltejs/kit/vite";
+import preprocess from "svelte-preprocess";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  preprocess: vitePreprocess(),
+  preprocess: preprocess(),
 
   kit: {
-    adapter: adapter({
-      pages: "build",
-      assets: "build",
-      fallback: false,
-      precompress: false,
-    }),
+    adapter: adapter(),
     paths: {
-      base: process.env.NODE_ENV === "production" ? "" : "",
+      base:
+        process.env.NODE_ENV === "production"
+          ? "/virtual-scheduling-assistant"
+          : "",
     },
   },
 };
